aa5960de16db963b89ade403df02ac565b9bfcf1
Kelly <38098037+kfragkog@users.noreply.github.com>
2018-12-25 16:32:13 +0000

    file ext updates

---
+++ reverseAString.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+  let a = [];
+  let revStr = "";
+
+  for (let i = 1; i <= str.length; i++){
+    a.push(str[str.length - i]);
+  }
+
+  revStr = a.join('');
+  console.log(revStr);
+  return revStr;
+}
+
+//example
+reverseString("hello");
